c1dae64b5a9c508fe449fab22424ebe104a5b8cf
sun ybin <ybin.sun@gmail.com>
2020-02-06 16:25:55 +0800

    reformat code

--- model.h
+++ model.h
@@ -1,5 +1,6 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
+
 #include <vector>
 #include <string>
 #include "geometry.h"
@@ -14,20 +15,34 @@ private:
     TGAImage diffusemap_;
     TGAImage normalmap_;
     TGAImage specularmap_;
+
     void load_texture(std::string filename, const char *suffix, TGAImage &img);
+
 public:
     Model(const char *filename);
+
     ~Model();
+
     int nverts();
+
     int nfaces();
+
     Vec3f normal(int iface, int nthvert);
+
     Vec3f normal(Vec2f uv);
+
     Vec3f vert(int i);
+
     Vec3f vert(int iface, int nthvert);
+
     Vec2f uv(int iface, int nthvert);
+
     TGAColor diffuse(Vec2f uv);
+
     float specular(Vec2f uv);
+
     std::vector<int> face(int idx);
 };
+
 #endif //__MODEL_H__
 
